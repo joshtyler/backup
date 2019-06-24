7a5392a6629c11df9fb83d965939978f22757537
Joshua Tyler <josh.tyler@btinternet.com>
2019-06-24 22:21:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-sudo cp backup_script_private.sh /usr/bin
-sudo cp backup_script.sh /usr/bin
-sudo cp backup.sh /usr/bin
-sudo cp restore.sh /usr/bin
-sudo cp backup.service /etc/systemd/system/
-sudo cp backup.timer /etc/systemd/system/
+if [ ! -f backup_script_private.sh ]; then
+	echo "Please create (and make executable) backup_script_private.sh (see backup_script.sh)"
+	exit 1
+fi
+
+set -x
+sudo cp backup_script_private.sh backup_script.sh backup.sh restore.sh /usr/bin
+sudo cp backup.service  backup.timer /etc/systemd/system/
+set +x
+echo "All done. You probably want to test it out by running systemctl start backup.service"
+echo "Once that is working, run systemctl enable backup.timer && systemctl start backup.timer"
